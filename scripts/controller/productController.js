70e306d6b4040182ea9191c9a47df6f3370e24fa
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2024-05-09 23:11:22 +0530

    design changes

--- scripts/controller/productController.js
+++ scripts/controller/productController.js
@@ -73,31 +73,32 @@ function displayProduct(prod) {
   const outputDiv = document.querySelector("#output");
 
   const colDiv = document.createElement("div");
-  colDiv.className = " card col-span-1";
+  colDiv.className = " card col-span-1 flex flex-col items-center ";
   outputDiv.appendChild(colDiv);
 
   const image = document.createElement("img");
   //image.className="h-24 w-full"
   image.src = prod.image;
+  image.className="w-full h-72"
   colDiv.appendChild(image);
 
   const textDiv = document.createElement("div");
-  textDiv.className = "m-4";
+  textDiv.className = "m-4 ";
   colDiv.appendChild(textDiv);
 
   const spanName = document.createElement("span");
-  spanName.classList.add("font-extrabold", "block");
+  spanName.classList.add("font-extrabold", "font-Macondo","text-xl", "block","text-[#255c5d]");
   spanName.innerText = prod.name;
   textDiv.appendChild(spanName);
 
   const spanDesc = document.createElement("span");
-  spanDesc.className = "block";
+  spanDesc.className = "block text-zinc-500 font-Lilita ";
   spanDesc.innerText = prod.description;
   textDiv.appendChild(spanDesc);
 
   const spanPrice = document.createElement("span");
-  spanPrice.className = "block";
-  spanPrice.innerText = prod.price;
+  spanPrice.className = "block text-3xl text-[#255c5d] mt-3 font-Macondo font-extrabold";
+  spanPrice.innerText = ` ₹ ${prod.price}`;
   textDiv.appendChild(spanPrice);
 
   const button = document.createElement("button");
@@ -106,8 +107,8 @@ function displayProduct(prod) {
   button.setAttribute('product-id',prod.id)
   button.addEventListener('click',addToCart)
   button.className =
-    "bg-blue-700 rounded-2xl px-8 py-2 mx-16  text-center mb-6";
-  button.innerText = "Add to Cart";
+    "bg-yellow-400 rounded-2xl px-8 py-2 text-white font-Lilita text-center mb-6";
+  button.innerText = "Fill Basket";
   colDiv.appendChild(button);
 
 }
